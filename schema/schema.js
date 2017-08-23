cb4c4ab66020076a956bbff8574140624871d1d9
Vick Kumar <vick.kumar44@gmail.com>
2017-08-23 14:28:47 -0400

    connected companies to users

--- schema/schema.js
+++ schema/schema.js
@@ -5,16 +5,41 @@ const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: {type: GraphQLString},
+    name: {type: GraphQLString},
+    description: {type: GraphQLString},
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args){
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/Users`)
+        .then(res => res.data);
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
-    age: {type: GraphQLInt}
-  }
+    age: {type: GraphQLInt},
+    company: {
+      type: CompanyType,
+      resolve(parentValue, arrgs){
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -26,6 +51,31 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
           .then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: {id: {type: GraphQLString}},
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLString)},
+        companyId: {type: GraphQLString}
+      },
+      resolve() {
+
       }
     }
   }
